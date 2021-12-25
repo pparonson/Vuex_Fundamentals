48ac897547d97492b74555390cb2faf1d5a3304a
paronson <paronson@qw-corp.com>
2021-12-25 11:36:14 -0600

    refactor to use Vuex state management

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,5 @@
 import { createStore } from 'vuex'
-
+import EventService from '@/services/EventService'
 export default createStore({
   state: {
     user: 'Adam Jahr',
@@ -8,8 +8,42 @@ export default createStore({
   mutations: {
     ADD_EVENT(state, event) {
       state.events.push(event)
+    },
+    SET_EVENTS(state, events) {
+      this.events = events
+    },
+    SET_EVENT(state, event) {
+      this.event = event
     }
   },
-  actions: {},
+  actions: {
+    createEvent({ commit }, event) {
+      EventService.postEvent(event)
+        .then(() => {
+          commit('ADD_EVENT', event)
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    },
+    fetchEvents({ commit }) {
+      EventService.getEvents()
+        .then(response => {
+          commit('SET_EVENTS', response.data)
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    },
+    fetchEvent({ commit }, id) {
+      EventService.getEvent(id)
+        .then(response => {
+          commit('SET_EVENT', response.data)
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    }
+  },
   modules: {}
 })
--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,71 +1,52 @@
 <template>
-<h1>Create an event</h1>
+  <h1>Create an event</h1>
 
-<div class="form-container">
+  <div class="form-container">
+    <form @submit.prevent="onSubmit">
+      <label>Select a category: </label>
+      <select v-model="event.category">
+        <option
+          v-for="option in categories"
+          :value="option"
+          :key="option"
+          :selected="option === event.category"
+          >{{ option }}</option
+        >
+      </select>
 
-  <form @submit.prevent="onSubmit">
-    <label>Select a category: </label>
-    <select v-model="event.category">
-      <option
-        v-for="option in categories"
-        :value="option"
-        :key="option"
-        :selected="option === event.category"
-      >{{ option }}</option>
-    </select>
+      <h3>Name & describe your event</h3>
 
-    <h3>Name & describe your event</h3>
+      <label>Title</label>
+      <input v-model="event.title" type="text" placeholder="Title" />
 
-    <label>Title</label>
-    <input
-      v-model="event.title"
-      type="text"
-      placeholder="Title"
-    >
+      <label>Description</label>
+      <input
+        v-model="event.description"
+        type="text"
+        placeholder="Description"
+      />
 
-    <label>Description</label>
-    <input
-      v-model="event.description"
-      type="text"
-      placeholder="Description"
-    />
+      <h3>Where is your event?</h3>
 
-    <h3>Where is your event?</h3>
+      <label>Location</label>
+      <input v-model="event.location" type="text" placeholder="Location" />
 
-    <label>Location</label>
-    <input
-      v-model="event.location"
-      type="text"
-      placeholder="Location"
-    />
+      <h3>When is your event?</h3>
+      <label>Date</label>
+      <input v-model="event.date" type="text" placeholder="Date" />
 
-    <h3>When is your event?</h3>
-    <label>Date</label>
-    <input
-      v-model="event.date"
-      type="text"
-      placeholder="Date"
-    />
+      <label>Time</label>
+      <input v-model="event.time" type="text" placeholder="Time" />
 
-    <label>Time</label>
-    <input
-      v-model="event.time"
-      type="text"
-      placeholder="Time"
-    />
-
-    <button type="submit">Submit</button>
-  </form>
-
-</div>
+      <button type="submit">Submit</button>
+    </form>
+  </div>
 </template>
 
 <script>
 import { v4 as uuidv4 } from 'uuid'
-import EventService from '@/services/EventService.js'
-
 export default {
-  data () {
+  data() {
     return {
       categories: [
         'sustainability',
@@ -95,14 +76,9 @@ export default {
         id: uuidv4(),
         organizer: this.$store.state.user
       }
-			EventService.postEvent(event)
-		  .then(() => {
-		    this.$store.commit('ADD_EVENT', event)
-	    })
-	    .catch(error => {
-		    console.log(error)
-	    })
-		}
+      this.$store.dispatch('createEvent', event);
+    },
   }
 }
 </script>
+
